b40e83b5e718eb2174f29260489115bf5476b7e7
soniyaparmar <132670133+soniyaparmar@users.noreply.github.com>
2023-09-16 10:40:42 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).keypress(function() {
+$("#bt").click(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
